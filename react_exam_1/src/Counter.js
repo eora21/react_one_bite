0df2a3c8d093071a0ebf4901e0094f434d826bd8
eora21 <eora21@naver.com>
2023-12-30 17:30:52 +0900

    feat: 🎸 추가 화면 렌더링 확인을 위해 콘솔 출력 작성

--- react_exam_1/src/Counter.js
+++ react_exam_1/src/Counter.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 
 const Counter = () => {
+  console.log("Counter Render");
+
   const [count, setCount] = useState(0);
   // useState는 배열을 반환하는데, 0번째는 상태값이고 1번째는 상태를 변화시키는 상태변화 함수이다.
   // 건네준 0은 초기값이다.
