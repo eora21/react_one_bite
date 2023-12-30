551a523d16108bc7a8b88e1189f1199a99eb085c
eora21 <eora21@naver.com>
2023-12-30 18:19:52 +0900

    refactor: 💡 수정 props를 직접 작성(비구조화 할당)하여 더 쉽게 사용할 수 있도록 변경

--- react_exam_1/src/Counter.js
+++ react_exam_1/src/Counter.js
@@ -1,12 +1,12 @@
 import React, {useState} from "react";
 
-const Counter = (props) => {
-  const [count, setCount] = useState(props.initValue);
+const Counter = ({initValue, plusValue, minusValue}) => {
+  const [count, setCount] = useState(initValue);
   // useState는 배열을 반환하는데, 0번째는 상태값이고 1번째는 상태를 변화시키는 상태변화 함수이다.
   // 건네준 0은 초기값이다.
 
-  const plus = () => setCount(count + props.plusValue);
-  const minus = () => setCount(count - props.minusValue);
+  const plus = () => setCount(count + plusValue);
+  const minus = () => setCount(count - minusValue);
 
   return (
     <div>
