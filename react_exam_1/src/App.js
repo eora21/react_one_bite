ea4c8ea86b6e01eb8c650587a5ca4cea43ddea2b
eora21 <eora21@naver.com>
2023-12-30 16:45:53 +0900

    refactor: 💡 수정 js를 html에 적용할 수 있는 jsx 확인

--- react_exam_1/src/App.js
+++ react_exam_1/src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
 
 function App() {
+  let name = "eora21";
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2> 안녕 리액트 {name}</h2>
       </header>
     </div>
   );
