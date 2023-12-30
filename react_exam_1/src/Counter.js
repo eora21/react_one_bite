57c0d5c25fa04093d0d11c734c961e1366df9e08
eora21 <eora21@naver.com>
2023-12-30 17:38:06 +0900

    refactor: 💡 수정 Counter가 props를 받아 적용하도록 변경
    props는 properties의 줄임말

--- react_exam_1/src/Counter.js
+++ react_exam_1/src/Counter.js
@@ -1,9 +1,7 @@
 import React, {useState} from "react";
 
-const Counter = () => {
-  console.log("Counter Render");
-
-  const [count, setCount] = useState(0);
+const Counter = (props) => {
+  const [count, setCount] = useState(props.initValue);
   // useState는 배열을 반환하는데, 0번째는 상태값이고 1번째는 상태를 변화시키는 상태변화 함수이다.
   // 건네준 0은 초기값이다.
 
