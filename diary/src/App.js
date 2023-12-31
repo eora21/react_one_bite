ce4a930d7626f40d0d01373719777c825fe589d2
eora21 <eora21@naver.com>
2023-12-31 17:10:01 +0900

    feat: 🎸 추가 api 이용하여 초기 데이터 채우도록 작성

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {Lifecycle} from "./Lifecycle";
 
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments")
+      .then(res => res.json());
+
+    const initData = res.slice(0, 20).map(data => {
+      return {
+        author: data.email,
+        content: data.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      }
+    });
+
+    setData(initData);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
