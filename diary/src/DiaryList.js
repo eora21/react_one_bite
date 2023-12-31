0f619fc04266774408a5674b8df30420b38e84b9
eora21 <eora21@naver.com>
2023-12-31 16:15:34 +0900

    feat: 🎸 추가 내용 수정 이벤트 전달

--- diary/src/DiaryList.js
+++ diary/src/DiaryList.js
@@ -1,12 +1,12 @@
 import {DiaryItem} from "./DiaryItem";
 
-const DiaryList = ({diaryList, onRemove}) => {
+const DiaryList = ({diaryList, onRemove, onEdit}) => {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
-        {diaryList.map((diary) => <DiaryItem key={diary.id} {...diary} onRemove={onRemove}/>)}
+        {diaryList.map((diary) => <DiaryItem key={diary.id} {...diary} onRemove={onRemove} onEdit={onEdit}/>)}
       </div>
     </div>
   )
