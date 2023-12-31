f1cf0a67922fc25009d8b1b0408a9d64d30a254c
eora21 <eora21@naver.com>
2023-12-31 13:30:10 +0900

    feat: 🎸 추가 일기 목록 사용
    더미 데이터 작성

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "김주호",
+    content: "더미1",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "eora21",
+    content: "더미2",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "김주호",
+    content: "더미3",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+]
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor/>
+      <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
