2c009edfb5e7d018c04bb9ec04642416296d7478
eora21 <eora21@naver.com>
2023-12-31 13:55:47 +0900

    feat: 🎸 추가 일기 목록은 useState 각각의 id는 useRef로 관리, 생성 이벤트 prop

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,36 +1,29 @@
 import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-
-const dummyList = [
-  {
-    id: 1,
-    author: "김주호",
-    content: "더미1",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "eora21",
-    content: "더미2",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "김주호",
-    content: "더미3",
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-]
+import {useRef, useState} from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current++
+    }
+
+    setData([newItem, ...data,])
+  };
+
   return (
     <div className="App">
-      <DiaryEditor/>
-      <DiaryList diaryList={dummyList}/>
+      <DiaryEditor onCreate={onCreate}/>
+      <DiaryList diaryList={data}/>
     </div>
   );
 }
