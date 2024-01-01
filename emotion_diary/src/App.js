1d7ce738a5f24aa8ce5726826f7c92e455a30a6a
eora21 <eora21@naver.com>
2024-01-01 11:15:28 +0900

    feat: 🎸 추가 버튼 변경 확인

--- emotion_diary/src/App.js
+++ emotion_diary/src/App.js
@@ -6,21 +6,20 @@ import {Edit} from "./pages/Edit";
 import {Diary} from "./pages/Diary";
 import {RouteTest} from "./components/RouteTest";
 
+// COMPONENTS
+import {MyButton} from "./components/MyButton";
+
 function App() {
-  // env.PUBLIC_URL이 작동하지 않을 경우
-  // const env = process.env;
-  // env.PUBLIC_URL = env.PUBLIC_URL || "";
 
   return (
     <BrowserRouter>
       <div className="App">
         <h1>감정 일기장</h1>
 
-        <img src={process.env.PUBLIC_URL + "/assets/emotion1.png"}/>
-        <img src={process.env.PUBLIC_URL + "/assets/emotion2.png"}/>
-        <img src={process.env.PUBLIC_URL + "/assets/emotion3.png"}/>
-        <img src={process.env.PUBLIC_URL + "/assets/emotion4.png"}/>
-        <img src={process.env.PUBLIC_URL + "/assets/emotion5.png"}/>
+        <MyButton text={"버튼"} type={"positive"} onClick={() => alert("버튼 클릭")}/>
+        <MyButton text={"버튼"} type={"negative"} onClick={() => alert("버튼 클릭")}/>
+        <MyButton text={"버튼"} type={"assafwds"} onClick={() => alert("버튼 클릭")}/>
+        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")}/>
 
         <Routes>
           <Route path="/" element={<Home/>}/>
