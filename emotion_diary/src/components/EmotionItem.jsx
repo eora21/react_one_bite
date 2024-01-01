d9ec57c77ba5fe6d6f7578836bfa7bdd860fea1f
eora21 <eora21@naver.com>
2024-01-02 08:46:01 +0900

    feat: 🎸 추가 감정 항목 작성

---
+++ emotion_diary/src/components/EmotionItem.jsx
@@ -0,0 +1,10 @@
+export const EmotionItem = ({emotion_id, emotion_img, emotion_descript, onClick, isSelected}) => {
+  return (
+    <div
+      className={["EmotionItem", isSelected ? `EmotionItem_on_${emotion_id}` : `EmotionItem_off`].join(" ")}
+      onClick={() => onClick(emotion_id)}>
+      <img src={emotion_img} alt="emotion"/>
+      <span>{emotion_descript}</span>
+    </div>
+  )
+}
