fc72b4f31465b585445049a1211d9f3ae98f0fad
eora21 <eora21@naver.com>
2024-01-01 22:51:47 +0900

    feat: 🎸 추가 일기 작성에서 일기 에디터 사용

--- emotion_diary/src/pages/New.jsx
+++ emotion_diary/src/pages/New.jsx
@@ -1,8 +1,9 @@
+import {DiaryEditor} from "../components/DiaryEditor";
+
 export const New = () => {
   return (
-    <div>
-      <h1>New</h1>
-      <p>이곳은 New입니다.</p>
+    <div className="New">
+      <DiaryEditor/>
     </div>
   )
 }
