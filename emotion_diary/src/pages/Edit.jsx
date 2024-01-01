7824c4f936f11fcc5107b5cd4181fd9517b62a3f
eora21 <eora21@naver.com>
2024-01-01 09:33:19 +0900

    feat: 🎸 추가 useNavigate 사용해서 페이지 이동
    하이퍼링크가 아닌 특정 로직에서도 이용이 가능, -1로 이전 페이지 이동도 가능

--- emotion_diary/src/pages/Edit.jsx
+++ emotion_diary/src/pages/Edit.jsx
@@ -1,7 +1,8 @@
-import {useSearchParams} from "react-router-dom";
+import {useNavigate, useSearchParams} from "react-router-dom";
 
 export const Edit = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
 
   const id = searchParams.get("id");
   const mode = searchParams.get("mode");
@@ -12,6 +13,8 @@ export const Edit = () => {
       <h1>Edit</h1>
       <p>이곳은 Edit입니다.</p>
       <button onClick={() => setSearchParams({who: "eora21"})}>QS</button>
+      <button onClick={() => navigate("/")}>home</button>
+      <button onClick={() => navigate(-1)}>뒤로 가기</button>
     </div>
   )
 }
