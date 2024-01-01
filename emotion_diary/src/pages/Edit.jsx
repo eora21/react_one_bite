b24d63bb77096ba2ccd40b44792b208ebb6c8d8f
eora21 <eora21@naver.com>
2024-01-01 09:27:58 +0900

    feat: 🎸 추가 useSearchParams를 이용하여 쿼리스트링 획득
    setSearchParams를 이용하여 쿼리스트링 변경도 가능

--- emotion_diary/src/pages/Edit.jsx
+++ emotion_diary/src/pages/Edit.jsx
@@ -1,8 +1,17 @@
+import {useSearchParams} from "react-router-dom";
+
 export const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+  console.log(id, mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 Edit입니다.</p>
+      <button onClick={() => setSearchParams({who: "eora21"})}>QS</button>
     </div>
   )
 }
