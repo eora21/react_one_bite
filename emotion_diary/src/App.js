83d99b8abbc7d441fb5b26ea6b971755f48a9985
eora21 <eora21@naver.com>
2024-01-01 10:54:43 +0900

    feat: 🎸 추가 process.env.PUBLIC_URL을 이용하여 이미지 적용 확인

--- emotion_diary/src/App.js
+++ emotion_diary/src/App.js
@@ -7,10 +7,21 @@ import {Diary} from "./pages/Diary";
 import {RouteTest} from "./components/RouteTest";
 
 function App() {
+  // env.PUBLIC_URL이 작동하지 않을 경우
+  // const env = process.env;
+  // env.PUBLIC_URL = env.PUBLIC_URL || "";
+
   return (
     <BrowserRouter>
       <div className="App">
         <h1>감정 일기장</h1>
+
+        <img src={process.env.PUBLIC_URL + "/assets/emotion1.png"}/>
+        <img src={process.env.PUBLIC_URL + "/assets/emotion2.png"}/>
+        <img src={process.env.PUBLIC_URL + "/assets/emotion3.png"}/>
+        <img src={process.env.PUBLIC_URL + "/assets/emotion4.png"}/>
+        <img src={process.env.PUBLIC_URL + "/assets/emotion5.png"}/>
+
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/new" element={<New/>}/>
