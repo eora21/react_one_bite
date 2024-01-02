aa8f90959ddae30d3b605cba4c804640500c0666
eora21 <eora21@naver.com>
2024-01-02 14:57:17 +0900

    refactor: 💡 수정 EmotionItem 메모이제이션

--- emotion_diary/src/components/EmotionItem.jsx
+++ emotion_diary/src/components/EmotionItem.jsx
@@ -1,4 +1,6 @@
-export const EmotionItem = ({emotion_id, emotion_img, emotion_descript, onClick, isSelected}) => {
+import React from "react";
+
+export const EmotionItem = React.memo(({emotion_id, emotion_img, emotion_descript, onClick, isSelected}) => {
   return (
     <div
       className={["EmotionItem", isSelected ? `EmotionItem_on_${emotion_id}` : `EmotionItem_off`].join(" ")}
@@ -7,4 +9,4 @@ export const EmotionItem = ({emotion_id, emotion_img, emotion_descript, onClick,
       <span>{emotion_descript}</span>
     </div>
   )
-}
+});
